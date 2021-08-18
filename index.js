129946f16753c173f806f3ed833371d58c3e85de
khoanguyen19 <61375967+khoanguyen19@users.noreply.github.com>
2021-08-19 00:26:47 +0700

    3.1 to 3.6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const persons = [
+let persons = [
     { 
         "id": 1,
         "name": "Arto Hellas", 
@@ -29,3 +29,79 @@ const persons = [
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
 })
+
+app.get('/info', (req, res) => {
+    const date = new Date();
+    const message = `
+        <p>Phone book has info for ${persons.length} people</p>
+        <p>${date}</p>
+    `
+    res.send(message);
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+    if(person) {
+        res.json(person);
+    } else {
+        res.status(404).send('Not Found');
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id);
+    
+    res.status(204).end()
+})
+
+const generateId = () => {
+    let id = 1;
+    const idArray = persons.map(person => person.id);
+    while(idArray.includes(id)) {
+        id = Math.floor(Math.random() * 1000000);
+    }
+     
+    return id;
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    const name = body.name;
+    const number = body.number;
+    const checkName = persons
+        .map(person => person.name.toUpperCase())
+        .includes(name.trim().toUpperCase());
+    console.log(checkName);
+    if(checkName){
+        return res.status(400).json({
+            error: 'Name must be unique'
+        });
+    }
+    if(!name) {
+        return res.status(400).json({
+            error: 'Missing name'
+        });
+    }
+    if(!number) {
+        return res.status(400).json({
+            error: 'Missing number'
+        });
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+    persons = persons.concat(person)
+    res.json(person);
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`listening on port ${PORT}`);
