87e1e0ef026221c614518ce2f6f5200ac1525b7d
khoanguyen19 <61375967+khoanguyen19@users.noreply.github.com>
2021-08-19 20:14:25 +0700

    final

--- index.js
+++ index.js
@@ -36,10 +36,6 @@ let persons = [
     }
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-})
-
 app.get('/info', (req, res) => {
     const date = new Date();
     const message = `
